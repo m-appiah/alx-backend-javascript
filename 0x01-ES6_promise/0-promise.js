7ff7d8701d10f0da77b3b99e744a9c6d147ebe6f
Michael Appiah <mappiahca@gmail.com>
2024-10-14 04:48:48 +0100

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // Simulate API call
+    resolve('This is a simulated response.');
+  });
+}
+export default getResponseFromAPI;
