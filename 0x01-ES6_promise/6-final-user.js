a212606c215294f4da861d8bf67754db0303eaec
Michael Appiah <mappiahca@gmail.com>
2024-10-14 05:59:48 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.reason || result.value,
-    })));
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      }
+      // Ensure the error message is correctly extracted for rejected promises
+      return { status: 'rejected', value: result.reason.message };
+    }));
 }
